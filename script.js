f0221479d8897b315d66484ae4decd396eaf2112
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2021-04-27 15:58:27 +0700

    commit

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-
-// Скрипт для Раскрыв. кнопки
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+var result = "<table border=1>";
+for (var i = 0; i < 10; i++) {
+    result += "<tr>";
+    for (var j = 0; j < 10; j++) {
+        result += "<td>" + '-------' + "</td>";
     }
-  }
+    result += "</tr>";
 }
-
-var result = "<table border=0>";
-    for(var i=0; i<10; i++) {
-        result += "<tr>";
-        for(var j=0; j<10; j++){
-            result += "<td>"+'-------'+"</td>";
-        }
-        result += "</tr>";
-    }
-    result += "</table>";
+result += "</table>";
 
 document.getElementById('cont').innerHTML = result;
 
-function firstTable(){
+function firstTable() {
     let arr1 = [];
     let maxArr1;
     let funsArr1 = [];
@@ -37,7 +18,7 @@ function firstTable(){
         arr1.push(Number(document.getElementById(`a${i}`).value));
     }
 
-    arr1 = subtractionArray(arr1);
+    arr1 = subtractionArrayFunS(arr1);
 
     maxArr1 = maxElemOf2dArray(arr1)
 
@@ -46,10 +27,10 @@ function firstTable(){
     makeTableHTML(funsArr1)
 
     properties(funsArr1)
-    
+
 }
 
-function secondTable(){
+function secondTable() {
     let arr2 = [];
     let funtArr2 = [];
     let maxArr2;
@@ -58,24 +39,28 @@ function secondTable(){
         arr2.push(Number(document.getElementById(`b${i}`).value));
     }
 
-    arr2 = subtractionArray(arr2);
+    arr2 = subtractionArrayFunT(arr2);
 
     maxArr2 = maxElemOf2dArray(arr2);
-    minArr2 = -maxArr2;
-    funtArr2 = arr2.map((arr) => arr.map((a) => (FunT(a, minArr2, maxArr2).toFixed(3))));
-    
+
+    console.log(minArr2)
+    funtArr2 = arr2.map((arr) => arr.map((a) => (FunT(a, maxArr2).toFixed(3))));
+
     makeTableHTML(funtArr2);
 
     properties(funtArr2)
 
 }
-function subtractionArray(arr){
-    tmpArr = [[]];
+
+function subtractionArrayFunS(arr) {
+    tmpArr = [
+        []
+    ];
     for (let i = 0; i < arr.length; i++) {
         tmpArr[i] = [];
 
         for (let j = 0; j < arr.length; j++) {
-            res = arr[i] - arr[j];
+            res = (arr[i] - arr[j]);
             tmpArr[i][j] = Number(res.toFixed(3));
 
         }
@@ -83,7 +68,23 @@ function subtractionArray(arr){
     return tmpArr
 }
 
-function maxElemOf2dArray(arr){
+function subtractionArrayFunT(arr) {
+    tmpArr = [
+        []
+    ];
+    for (let i = 0; i < arr.length; i++) {
+        tmpArr[i] = [];
+
+        for (let j = 0; j < arr.length; j++) {
+            res = Math.abs(arr[i] - arr[j]);
+            tmpArr[i][j] = Number(res.toFixed(3));
+
+        }
+    }
+    return tmpArr
+}
+
+function maxElemOf2dArray(arr) {
     max = arr[0][0]
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
@@ -93,32 +94,29 @@ function maxElemOf2dArray(arr){
     return max
 }
 
-function FunS(a, max){
+function FunS(a, max) {
     mean = max / 2
     if (a < 0) return 0
     else if (a >= 0 && a <= mean) return 2 * (a / max) ** 2
-    else if (a >= mean && a <= max) 
+    else if (a >= mean && a <= max)
         return 1 - 2 * ((a - max) / (max - 0) * (a - max) / (max - 0))
     else if (a > max) return 1
 }
 
-function FunT(a, min, max){
-    if (a <= min) return 0
-    else if (a >= min && a <= 0) return (a + max) / max
-    else if (a >= 0 && a <= max) return (max - a) / max
-    else if (a >= max) return 0 
+function FunT(x, max) {
+    if (x >= 0 && x <= max) return (max - x) / max;
+    else return 0;
 }
 
 function makeTableHTML(myArray) {
-    var result = "<table border=0>";
+    var result = "<table border=1>";
     for (var i = 0; i < myArray.length; i++) {
         result += "<tr>";
         for (var j = 0; j < myArray[i].length; j++) {
-            if (i == j){
-                result += "<td bgcolor='red'>" + myArray[i][j] + "</td>";
-            }
-            else{
-                result += "<td style='background: #D8E6F3''>" + myArray[i][j] + "</td>";
+            if (i == j) {
+                result += "<td bgcolor='lightblue'>" + myArray[i][j] + "</td>";
+            } else {
+                result += "<td>" + myArray[i][j] + "</td>";
             }
 
         }
@@ -130,7 +128,7 @@ function makeTableHTML(myArray) {
 }
 
 
-function properties(arr){
+function properties(arr) {
 
     // рефлексивность
     refl = true;
@@ -141,35 +139,35 @@ function properties(arr){
         if (arr[i][i] != 1) {
             refl = false;
             break
-        }  
+        }
     }
-    
-    if (refl){
+
+    if (refl) {
         strongAntiRefl = false;
         outerLoop: for (let i = 0; i < arr.length; i++) {
             for (let j = 0; j < arr.length; j++) {
-                if ( i != j ){
-                    if (arr[i][j] == 1){
+                if (i != j) {
+                    if (arr[i][j] == 1) {
                         strongRefl = false;
                         break outerLoop
                     }
                 }
-            } 
+            }
         }
-    }
-    else{
+    } else {
         strongRefl = false;
         outerLoop: for (let i = 0; i < arr.length; i++) {
             for (let j = 0; j < arr.length; j++) {
-                if ( i != j ){
-                    if (arr[i][j] == 0){
+                if (i != j) {
+                    if (arr[i][j] == 0) {
                         strongAntiRefl = false;
                         break outerLoop
                     }
                 }
-            } 
+            }
         }
     }
+
 
     // симметричность
 
@@ -180,27 +178,26 @@ function properties(arr){
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
 
-            if (arr[i][j] != arr[j][i]){
+            if (arr[i][j] != arr[j][i]) {
 
                 simm = false;
             }
 
-            if (i != j){  
-
-                if (arr[i][j] != 0 || arr[j][i] != 0){
-
-                    antiSimm = false; 
-
-                }
-            }
-
-            if (arr[i][j] != 0 || arr[j][i] != 0){
-
+            if (arr[i][i] == 1) {
                 aSimm = false;
 
+                if (Math.min(arr[i][j], arr[j][i]) != 0) {
+                    antiSimm = false;
+                }
+            } else {
+                antiSimm = false;
+
+                if (Math.min(arr[i][j], arr[j][i]) != 0) {
+                    aSimm = false;
+                }
             }
         }
-        
+
     }
 
     //транзитивность
@@ -209,15 +206,24 @@ function properties(arr){
     minArr = []
 
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) { 
+        for (let j = 0; j < arr.length; j++) {
             for (let k = 0; k < arr.length; k++) {
-                if (!(arr[i][k] >= arr[i][j] && arr[i][k] >= arr[j][k])){
-                    tranz = false;
+                minArr.push(Math.min(arr[i][k], arr[k][j]));
+
+            }
+            mm = minArr[0];
+
+            for (let l = 1; l < arr.length; l++) {
+                if (mm < minArr[l]) {
+                    mm = minArr[l];
                 }
-            } 
+            }
+
+            if (arr[i][j] < mm) {
+                tranz = false;
+            }
         }
     }
-    
 
     // линейность
 
@@ -226,16 +232,17 @@ function properties(arr){
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-            if ( !( arr[i][j] == 1 || arr[j][i] == 1 ) ){
+            if (!(arr[i][j] == 1 || arr[j][i] == 1)) {
                 strongLinear = false;
             }
-            if ( !( arr[i][j] > 0 || arr[j][i] > 0 ) ){
+            if (!(arr[i][j] > 0 || arr[j][i] > 0)) {
                 weakLinear = false;
             }
-        }        
+        }
     }
 
-    // вывод
+
+    // вывод в fieldset
 
     document.getElementById('prop1').innerHTML = refl ? (strongRefl ? 'Сильно' : 'Слабо') : 'Нет';
     document.getElementById('prop2').innerHTML = !refl ? (strongAntiRefl ? 'Сильно' : 'Слабо') : 'Нет';
@@ -244,6 +251,5 @@ function properties(arr){
     document.getElementById('prop5').innerHTML = aSimm ? 'Да' : 'Нет';
     document.getElementById('prop6').innerHTML = strongLinear ? 'Сильная' : (weakLinear ? 'Слабая' : 'Нет');
     document.getElementById('prop7').innerHTML = tranz ? 'Да' : 'Нет';
-    
-}
 
+}
